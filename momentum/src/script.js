93a4135ba0f0d1af8ca02b6fc316add13526fb25
MDz1985 <m_d@tut.by>
2021-10-25 00:16:35 +0300

    feat: quotes translate done

--- momentum/src/script.js
+++ momentum/src/script.js
@@ -7,6 +7,7 @@ const body = document.getElementsByTagName('body')[0];
 const leftSlideButton = document.querySelector('.slide-prev');
 const rightSlideButton = document.querySelector('.slide-next')
 
+const weather = document.querySelector('.weather');
 const weatherIcon = document.querySelector('.weather-icon');
 const temperature = document.querySelector('.temperature');
 const weatherDescription = document.querySelector('.weather-description');
@@ -40,6 +41,12 @@ const lang = document.querySelector('.lang');
 
 const menuButton = document.querySelector('.settings');
 const settingsMenu = document.querySelector('.nav');
+const menuTitle = settingsMenu.getElementsByTagName('h2')[0];
+const menuSecondTitle = settingsMenu.getElementsByTagName('h3')[0];
+const settingNames = settingsMenu.getElementsByTagName('span');
+
+// console.log(settingNames);
+const quotes = document.querySelector('.quotes');
 
 const offTime = document.getElementById('time');
 const offDate = document.getElementById('date');
@@ -119,6 +126,8 @@ function getLocalStorage() {
     if(localStorage.getItem('city')) {
         city.value = localStorage.getItem('city');
         setCity()
+    } else {
+        city.value = 'Minsk';
     }
 }
 window.addEventListener('load', getLocalStorage)
@@ -140,10 +149,10 @@ function setBg(){
     const dat =new Date()
     let timeOfDay = getTimeOfDay(dat.getHours());
     const img = new Image();
-    img.src = `https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
+    img.src = `https://raw.githubusercontent.com/MDz1985/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg`;
     img.addEventListener('load' , () => {
         // body.style.transition = '10s ease-in-out';
-        body.style.backgroundImage = `url('https://raw.githubusercontent.com/rolling-scopes-school/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
+        body.style.backgroundImage = `url('https://raw.githubusercontent.com/MDz1985/stage1-tasks/assets/images/${timeOfDay}/${bgNum}.jpg')`;
 
     });
 }
@@ -173,7 +182,7 @@ async function getWeather(language) {
     const url = `https://api.openweathermap.org/data/2.5/forecast?id=524901&q=${city.value}&lang=${language}&appid=cfe62a6645722176464f799a227a1ab5&units=metric`;
     const res = await fetch(url);
     const data = await res.json();
-    if (data.cod === '400' || data.city.population === 0){
+    if (data.cod === '404' || data.city.population === 0){
         if (language === 'en') {
             alert('Wrong city name or there is no information for this town!')
         } else {
@@ -181,13 +190,19 @@ async function getWeather(language) {
         }
     }
     let ms;
+    let windSpeed;
+    let humidityValue;
     if (language === 'ru')  {
         ms = ' м/с';
+        windSpeed = 'Скорость ветра ';
+        humidityValue = 'Влажность ';
         if (city.value === 'Minsk'){
             city.value = 'Минск';
         }
     } else {
         ms = ' m/s';
+        windSpeed = 'Wind speed ';
+        humidityValue = 'Humidity ';
         if (city.value === 'Минск'){
             city.value = 'Minsk';
         }
@@ -196,8 +211,8 @@ async function getWeather(language) {
     weatherIcon.classList.add(`owf-${data.list[0].weather[0].id}`);
     temperature.textContent = `${Math.round(data.list[0].main.temp * 10)/10}°C`;
     weatherDescription.textContent = data.list[0].weather[0].description;
-    wind.textContent = Math.round(data.list[0].wind.speed) + ms;
-    humidity.textContent = data.list[0].main.humidity + ' %';
+    wind.textContent = windSpeed + Math.round(data.list[0].wind.speed) + ms;
+    humidity.textContent = humidityValue + String(data.list[0].main.humidity) + ' %';
 }
 
 
@@ -219,18 +234,59 @@ city.addEventListener('mouseenter', () => {city.addEventListener('mouseleave', s
 
 
 //QUOTES
-async function getQuotes() {
-    const quotes = 'assets/quotes/data.json';
+async function getQuotes(number) {
+    let quotes;
+    if (lang.value === 'English'){
+        quotes = 'assets/quotes/quotesEn.json';
+    }else {
+        quotes = 'assets/quotes/quotesRu.json';
+    }
     const res = await fetch(quotes);
     const data = await res.json();
 
-    const number = getRandomNum(data.length);
+    if (number === -1){
+        number = getRandomNum(data.length);
+    }
+    localStorage.setItem('quoteNumber', number);
+
     quote.textContent = data[number].text;
     author.textContent = data[number].author;
 }
-getQuotes();
+//
+// async function getQuotesEn() {
+//
+//     const quotes = 'assets/quotes/quotesEn.json';
+//
+//     const res = await fetch(quotes);
+//     const data = await res.json();
+//
+//     const number = getRandomNum(data.length);
+//
+//     quote.textContent = data[number].text;
+//     author.textContent = data[number].author;
+// }
+// getQuotesEn().then();
+//
+// async function getQuotesRu() {
+//
+//     const quotes = 'assets/quotes/quotesRu.json';
+//
+//     const res = await fetch(quotes);
+//     const data = await res.json();
+//
+//     const number = getRandomNum(data.length);
+//
+//     quote.textContent = data[number].text;
+//     author.textContent = data[number].author;
+// }
+//
+function getQuotesRandom() {
+    getQuotes(-1).then()
+}
 
-changeQuote.addEventListener('click', getQuotes);
+// function getQuotes() {
+getQuotesRandom(-1);
+changeQuote.addEventListener('click', getQuotesRandom);
 
 
 
@@ -379,6 +435,10 @@ playList.forEach(el => {
     li.classList.add('playlist-item');
     li.textContent = el.title;
     playListContainer.append(li);
+    const span = document.createElement('span');
+    span.textContent = el.duration;
+    li.append(span);
+
 })
 
 //TRANSLATE
@@ -387,17 +447,39 @@ const greetingTranslation = {
     be: ['Добрай раніцы', 'Добры дзень', 'Добры вечар', 'Дабранач'],
     ru: ['Доброе утро', 'Добрый день', 'Добрый вечер', 'Доброй ночи']
 };
+const menuTranslation = {
+    en: ['Settings', 'Show', 'Time', 'Date', 'Greeting', 'Quotes', 'Weather', 'Player'],
+    ru: ['Настройки', 'Отображать', 'Время', 'Дата', 'Приветствие', 'Цитаты', 'Погода', 'Плеер']
+}
+
 lang.addEventListener('change', function (){
     if (lang.value === 'English'){
         weatherLang = 'en';
+        menuTitle.innerText = menuTranslation.en[0];
+        menuSecondTitle.innerText = menuTranslation.en[1];
+        for (let i = 0; i < settingNames.length; i++){
+            settingNames[i].textContent = menuTranslation.en[i+2];
+        }
+
 
     }else {
         weatherLang = 'ru';
+        menuTitle.innerText = menuTranslation.ru[0];
+        menuSecondTitle.innerText = menuTranslation.ru[1];
+        for (let i = 0; i < settingNames.length; i++){
+            settingNames[i].textContent = menuTranslation.ru[i+2];
+        }
     }
-    getWeather(weatherLang).then()
+    getWeather(weatherLang).then();
+    getQuotes(localStorage.getItem('quoteNumber')).then();
 });
-
+//
+// if (lang.value === 'English') {
+//     greeting.textContent = message;
+// }
 //MENU
+
+
 
 let visible = false
 function hideMenu(){
@@ -416,5 +498,34 @@ function hideItem(item){
     item.style.opacity = '0';
     item.style.userSelect = 'none';
 }
+function visibleItem(item){
+    item.style.opacity = '1';
+    item.style.userSelect = 'all';
+}
 
-offTime.addEventListener('change', () => {hideItem(time)});
+function listener(button, element){
+    button.addEventListener('change', () => {
+        if (!button.checked) {hideItem(element);
+        } else {
+            visibleItem(element);
+        }
+    });
+}
+
+listener(offTime, time);
+listener(offDate, date);
+listener(offGreeting, greeting);
+listener(offGreeting, name);
+listener(offPlayer, player);
+listener(offQuotes, quotes);
+listener(offQuotes, changeQuote);
+listener(offWeather, weather);
+
+
+offTime.addEventListener('change', () => {
+    if (!offTime.checked) {hideItem(time);
+    } else {
+        visibleItem(time);
+    }
+});
+
